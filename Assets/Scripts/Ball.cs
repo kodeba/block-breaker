ad3eba21de6130c467cef07edaf7d4a6702bb356
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-02-18 13:03:00 +0700

    feat: move paddle and made the ball stick with it, limit screen also

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private Paddle paddle;
+
+    Vector2 paddleToBallVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    private float screenWidthInUnits = 16f;
+
+    [SerializeField]
+    private float minX = 1f;
+
+    [SerializeField]
+    private float maxX = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        posX = Mathf.Clamp(posX, minX, maxX);
+        float posY = transform.position.y;
+        Vector2 pos = new Vector2(posX, posY);
+        transform.position = pos;
+    }
+}

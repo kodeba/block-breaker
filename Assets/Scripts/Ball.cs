051e1a92ec6937bf49faf3950eb306a7f3c7ca71
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-02-19 12:41:48 +0700

    feat: launch the ball on mouse click and create play space

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +8,42 @@ public class Ball : MonoBehaviour
     [SerializeField]
     private Paddle paddle;
 
+    [SerializeField]
+    private float xPush = 2f;
+
+    [SerializeField]
+    private float yPush = 15f;
+
+    private bool hasStarted;
+
     Vector2 paddleToBallVector;
     // Start is called before the first frame update
     void Start()
     {
+        hasStarted = false;
         paddleToBallVector = transform.position - paddle.transform.position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;

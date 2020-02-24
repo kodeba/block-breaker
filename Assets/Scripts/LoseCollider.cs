7421957caf1029279ed1f6c7449954c2852be650
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-02-24 12:47:40 +0700

    feat: add script to control game speed

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -7,7 +7,7 @@ public class LoseCollider : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("GameOver");
     }
 
 }
